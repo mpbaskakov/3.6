ce7d5b5d879b32f1638eb32d3bf36f3560eef22a
Maxim Baskakov <mpbaskakov@gmail.com>
2017-09-28 09:29:56 +0300

    Checked by the teacher #2

--- main.py
+++ main.py
@@ -1,56 +1,64 @@
 class Animal:
-    name = ''
-    isabird = False
-    color = ''
-    milk_production = 0 #litres in a day
-    eggs_production = 0 #pieces in a day
-    weight = 0 #kg
-    hunger = 0 #percent 0-100
-
+    def __init__(self, name, color, weight, hunger):
+        self.name = name
+        self.color = color
+        self.weight = weight
+        self.hunger = hunger
 
     def eat(self):
         self.hunger -= 10
         return self.hunger
 
 
-cow = Animal()
-cow.name = 'Cow'
-cow.color = 'White'
-cow.hunger = 100
-cow.weight = 750
-cow.milk_production = 10
+class Cow(Animal):
+    def __init__(self, name, color, milk_production, weight, hunger):
+        super().__init__(name, color, weight, hunger)
+        self.milk_production = milk_production
 
-goat = Animal()
-goat.name = 'Goat'
-goat.color = 'Grey'
-goat.milk_production = 5
-goat.hunger = 70
-goat.weight = 50
 
-pig = Animal()
-pig.name = 'Pig'
-pig.color = 'Pink'
-pig.hunger = 30
-pig.weight = 300
+class Goat(Animal):
+    def __init__(self, name, color, milk_production, weight, hunger):
+        super().__init__(name, color, weight, hunger)
+        self.milk_production = milk_production
 
-duck = Animal()
-duck.name = 'Duck'
-duck.isabird = True
-duck.color = 'Grey'
-duck.weight = 15
-duck.hunger = 35
 
-chicken = Animal()
-chicken.name = 'Chicken'
-chicken.isabird = True
-chicken.color = 'Orange'
-chicken.eggs_production = 2
-chicken.weight = 5
-chicken.hunger = 45
+class Pig(Animal):
+    def __init__(self, name, color,  weight, hunger):
+        super().__init__(name, color, weight, hunger)
 
-goose = Animal()
-goose.name = 'Goose'
-goose.isabird = True
-goose.color = 'Grey'
-goose.weight = 7
-goose.hunger = 15
+
+class Duck(Animal):
+    def __init__(self, name, color,  isabird, weight, hunger):
+        super().__init__(name, color, weight, hunger)
+        self.isabird = isabird
+
+
+class Chicken(Animal):
+    def __init__(self, name, color,  isabird, eggs_produciton, weight, hunger):
+        super().__init__(name, color, weight, hunger)
+        self.isabird = isabird
+        self.eggs_production = eggs_produciton
+
+
+class Goose(Animal):
+    def __init__(self, name, color,  isabird, weight, hunger):
+        super().__init__(name, color, weight, hunger)
+        self.isabird = isabird
+
+cow = Cow('Dusyua', 'White', 10, 750, 100)
+print(cow.color)
+
+goat = Goat('Goat', 'Grey', 5, 50, 70)
+print(goat.hunger)
+
+pig = Pig('Borya', 'Pink', 300, 30)
+print(pig.name)
+
+duck = Duck('Duck', 'Grey', True, 15, 35)
+print(duck.isabird)
+
+chicken = Chicken('Ryaba', 'Orange', True, 2, 5, 45)
+print(chicken.eggs_production)
+
+goose = Goose('Goose', 'Color', True, 7, 15)
+print(goose.weight)
